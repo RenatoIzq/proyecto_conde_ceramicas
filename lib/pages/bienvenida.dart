14d8ad20608de26a2fb291b7c8a37e3148c289b6
RenatoIzq <renatoizquierdo.ric@gmail.com>
2025-10-09 00:04:54 -0300

    Añadido el color gris oscuro y el container de la bienvenida

--- lib/pages/bienvenida.dart
+++ lib/pages/bienvenida.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:proyecto_conde_ceramicas/themes/themes.dart';
 
 class Bienvenida extends StatelessWidget {
   const Bienvenida({super.key});
@@ -21,17 +22,28 @@ class Bienvenida extends StatelessWidget {
               Center(
                 child: Image.asset(
                   'images/condeceramicalogo.png',
-                  width: 200,
-                  height: 200,
+                  width: 250,
+                  height: 250,
                 ),
               ),
               SizedBox(height: 20),
               Text(
                 'Bienvenido, seleccione una opción:',
                 textAlign: TextAlign.center,
-                style: GoogleFonts.oswald(fontSize: 30, color: Colors.white),
+                style: GoogleFonts.oswald(fontSize: 30, color: Colors.black),
               ),
               SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: grisoscuro,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
---
+++ lib/themes/themes.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var grisoscuro = Color.fromARGB(255, 66, 66, 66);
